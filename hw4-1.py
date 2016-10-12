ef314d33937508ca381550a6c591fbf2c994a2fa
Clayton Northey <clayton.northey@cnorthey.eprize.local>
2016-10-12 12:51:50 -0400

    plotting

--- hw4-1.py
+++ hw4-1.py
@@ -1,4 +1,5 @@
 import math
+from matplotlib import pyplot
 
 # rows
 FMHZ_100 = 0
@@ -19,18 +20,20 @@ DATA = [
 	[0.0016, 0.0037, 0.0021, 0.0015]
 ]
 
+FREQUENCY_RANGE = [f for f in range(1000, 100001)]
+
 def _get_loss(k1, k2, f, d):
 	return (k1 * math.sqrt(f) + k2 * f)
 
 def plot_loss(cable_id):
 	k1 = DATA[K1][cable_id]
 	k2 = DATA[K2][cable_id]
-	f_Mhz = [f for f in range(100, 100001)]
 	d = 1 #assuming this is one
-	y_axis = [_get_loss(k1, k2, f, d) for f in f_Mhz]
-	print(y_axis) 
+	y_axis = [_get_loss(k1, k2, f, d) for f in FREQUENCY_RANGE]
+	pyplot.plot(FREQUENCY_RANGE, y_axis)
 
 if __name__=='__main__':
 	for cable in [C625_F, C500_F, CRG_6, CRG_59]:
 		plot_loss(cable)
+	pyplot.show()
 
